f12188b4b23c5fa478a38612527bafcad3c88107
jodyboy <elite.crew66@gmail.com>
2024-11-23 21:20:45 +0700

    remove the recent commit to write from scratch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PatientModule } from './patient/patient.module';
 
 @Module({
-  imports: [PatientModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
